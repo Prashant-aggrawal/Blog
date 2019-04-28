1b2c1e24a3a650440a208a1a38ce38363d713b53
Prashant Agrawal <pamittal43@gmail.com>
2019-04-28 18:19:11 +0530

    Create Login and Signup

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from .import views
+from django.urls import path
+
+app_name='accounts'
+
+
+urlpatterns = [
+    url('signup', views.signup_view, name="signup"),
+    url('login', views.login_view, name="login"),
+]
--- dapp/urls.py
+++ dapp/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.conf.urls.static import static
+from  django.conf import settings
 from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,7 +27,9 @@ urlpatterns = [
     
     path('', views.home),
     path('articles/', include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
     
     
 ]
 urlpatterns +=staticfiles_urlpatterns()
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
